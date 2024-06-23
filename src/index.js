b6b65f507d8ef7ff9cddc667a73aacd23f04a515
MoTinny <150345083+Motinny@users.noreply.github.com>
2024-06-23 11:48:51 +0000

    added forecast HTML from Javascript

--- src/index.js
+++ src/index.js
@@ -17,6 +17,37 @@ function search(event) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="row">   
+     <div class="col-2"> 
+      <div class="weather-forecast-date">${day}</div> 
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M440-760v-160h80v160h-80Zm266 110-55-55 112-115 56 57-113 113Zm54 210v-80h160v80H760ZM440-40v-160h80v160h-80ZM254-652 140-763l57-56 113 113-56 54Zm508 512L651-255l54-54 114 110-57 59ZM40-440v-80h160v80H40Zm157 300-56-57 112-112 29 27 29 28-114 114Zm283-100q-100 0-170-70t-70-170q0-100 70-170t170-70q100 0 170 70t70 170q0 100-70 170t-170 70Zm0-80q66 0 113-47t47-113q0-66-47-113t-113-47q-66 0-113 47t-47 113q0 66 47 113t113 47Zm0-160Z"/></svg>  
+        <div class="weather-forecast-temperture">  
+            <span class="weather-forecast-temperature-max">     
+               <strong>  22°
+               </strong>
+               </span>                    
+           <span class="weather-forecast-temperature-min">     
+                  18°</span>               
+`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -51,3 +82,4 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+displayForecast();
